8ffc8028b83bea6f1731e367606486ae42fb8264
Bandit <125184568+heifengzhai@users.noreply.github.com>
2023-03-25 13:25:17 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,23 +5,9 @@ export default function Footer() {
     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
       <div>
         Powered by{" "}
-        <a
-          href="https://openai.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="font-bold hover:underline transition underline-offset-2"
-        >
-          OpenAI{" "}
-        </a>
-        and{" "}
-        <a
-          href="https://vercel.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="font-bold hover:underline transition underline-offset-2"
-        >
-          Vercel Edge Functions.
-        </a>
+       Ailvsuo.com.cn
+      
+       
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
         <Link
