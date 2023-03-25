19b6c8b005b0f38120d317c3a7a550d76bba47e4
Bandit <125184568+heifengzhai@users.noreply.github.com>
2023-03-25 18:12:54 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="text-center h-12 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-center items-center px-3 space-y-3 sm:mb-0 mb-3">
       
-      <div className="flex space-x-4 pb-4 sm:pb-0">
+      <div className="flex space-x-4 sm:pb-0">
         Powered by{" "}
        Ailvsuo.com.cn
       </div>
