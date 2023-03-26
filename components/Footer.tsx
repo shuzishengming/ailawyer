c46ec3aeb548fef27893403fa5243fb04c049497
Bandit <125184568+heifengzhai@users.noreply.github.com>
2023-03-26 10:07:40 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,10 +2,22 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="text-center h-12 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-center items-center px-3 space-y-3 sm:mb-0 mb-3">
+   
+    
+     <footer className="text-center h-16 sm:h-20 w-full sm:pt-2 pt-4 border-t mt-5 flex sm:flex-row flex-col justify-between items-center px-3 space-y-3 sm:mb-0 mb-3">
       
-      <div className="flex space-x-4 sm:pb-0">
-        Powered by{" "}
+      <div>
+        作者:台风(chatgpt魔法师 <a
+          href="http://wz.aidaohang.com.cn/taifeng/?f=tianzhiyuan"
+          target="_blank"
+          rel="noreferrer"
+          className="text-blue-500 hover:text-blue-700 hover:underline transition underline-offset-2"
+        >
+          了解更多
+        </a>)
+      </div>
+      <div className="flex space-x-4 pb-4 sm:pb-0">
+       Powered by{" "}
        Ailvsuo.com.cn
       </div>
     </footer>
